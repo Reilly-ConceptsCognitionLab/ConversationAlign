2bb7e98f0753a09da77fdb4a73d0dacd9d51d4da
Jamie Reilly <33383970+reilly-lab@users.noreply.github.com>
2025-07-05 15:38:53 -0400

    fixed bracketing error in lag corr helper function

---
+++ R/compute_lag_corr.R
@@ -0,0 +1,179 @@
+#'
+#' computes lagged correlations alignment measure across partners within each conversation
+#' @name compute_lagcorr
+#' @importFrom dplyr bind_rows
+#' @importFrom dplyr group_by
+#' @importFrom dplyr mutate
+#' @importFrom dplyr rename_with
+#' @importFrom dplyr select
+#' @importFrom dplyr summarise
+#' @importFrom magrittr %>%
+#' @importFrom tidyr pivot_wider
+#' @importFrom tidyselect contains
+#' @importFrom tidyselect ends_with
+#' @keywords internal
+#' @noRd
+#'
+compute_lagcorr <- function(df_prep, lags = c(-2, 0, 2)) {
+  #selects alignment dimensions based on prefixes rather than fixed list reference
+  align_var <- grep("^(emo_|lex_|sem_|phon_)", colnames(df_prep), value = TRUE, ignore.case = TRUE)
+
+  df_prep <- droplevels(df_prep)
+
+  #mutates an interlocutor pair variable, and preserves all columns
+  #add a column of concatenated interlocutor names by transcript
+  df_prep <- df_prep %>%
+    dplyr::group_by(Event_ID) %>%
+    dplyr::mutate(Participant_Pair = paste(sort(unique(Participant_ID)), collapse = "---")) %>%
+    dplyr::ungroup()
+
+  #DEFINE SPEARMAN CORRELATION FUNCTION
+  spearmans_corr_dyads <- function(df_prep) {
+
+    df_list <- split(df_prep, f = df_prep$Event_ID)
+    #iterate over each newly split data frame.
+    output_df_list <- lapply(df_list, function(df){
+
+      #establish raw participant name and S1/S2 'key' - denoted by order of utterance
+      participantvec <- unique(df$Participant_ID)
+
+      #substitute participant names with temp variable to be replaced with real names later
+      names(participantvec) <- c("S1", "S2")
+      df$Participant_ID <- gsub(participantvec[1], names(participantvec)[1], df$Participant_ID)
+      df$Participant_ID <- gsub(participantvec[2], names(participantvec)[2], df$Participant_ID)
+
+      #create a wide data frame with each row containing both participants' turn aggregated scores
+      df_wide <- df %>%
+        dplyr::group_by(Event_ID, Exchange_Count, Participant_ID, .add = FALSE) %>%
+        dplyr::summarise(dplyr::across(tidyselect::contains(align_var), ~ mean(.x, na.rm = TRUE)),
+                         .groups = "drop") %>%
+        tidyr::pivot_wider(names_from = tidyselect::contains("Participant_ID"),
+                           values_from = align_var)
+
+      # if there is only one aligned variable manually add that variable name to participant columns
+      if (length(align_var) == 1) {
+        colnames(df_wide)[which(colnames(df_wide) %in% c("S1", "S2"))] <- paste(align_var[1], colnames(df_wide)[which(colnames(df_wide) %in% c("S1", "S2"))], sep = "_")
+      }
+
+      df_wide <- df_wide %>%
+        dplyr::select(Event_ID, Exchange_Count, tidyselect::contains(align_var))
+
+      rows_with_na_ind <- apply(df_wide[,which(colnames(df_wide) %in% paste(align_var, "S1", sep = "_") |
+                                                 colnames(df_wide) %in% paste(align_var, "S2", sep = "_"))], 1, function(x){
+                                                   any(is.na(x) == TRUE & is.nan(x) == FALSE)
+                                                 })
+      # only slice out rows if there is at least one, otherwise it will not index any rows
+      if (any(rows_with_na_ind)) {
+        df_wide <- df_wide[!rows_with_na_ind, ]
+      }
+
+      # interpolate over turns that are fully missing (if all words in the utterance have been removed)
+      interp_df <- df_wide %>%
+        dplyr::mutate(dplyr::across(tidyselect::contains(align_var),
+                                    ~ zoo::na.approx(.x, na.rm = FALSE))) %>%
+        tidyr::fill(names(df_wide[, which(colnames(df_wide) %in% paste(align_var, "S1", sep = "_")|
+                                            colnames(df_wide) %in% paste(align_var, "S2", sep = "_"))]), .direction = "updown") # fills up or down for initial and ending values
+
+      x_vars <- interp_df %>%
+        dplyr::select(c('Event_ID') | c(tidyselect::ends_with("_S1"))) %>%
+        dplyr::rename_with(~stringr::str_replace(., "_S1", ""),
+                           tidyselect::ends_with("_S1"))
+      y_vars <- interp_df %>%
+        dplyr::select(c('Event_ID') | c(tidyselect::ends_with("_S2"))) %>%
+        dplyr::rename_with(~stringr::str_replace(., "_S2", ""),
+                           tidyselect::ends_with("_S2"))
+
+      # function to calculate and format spearman correlation
+      calculate_spearman_corr <- function(x_vars, y_vars, dim, align_var) {
+        tryCatch({
+          # pull the values for each participant out of the data frame as a vector
+          dim_x_vars <- x_vars[[dim]]
+          dim_y_vars <- y_vars[[dim]]
+
+          #run spearman corr and format rho and p value into a data frame - with complete cases
+          sc_results <- cor.test(dim_x_vars, dim_y_vars,
+                                 method = "spearman", exact = F,
+                                 na.action = "na.omit")
+
+          sc_results_df <- data.frame(SpearR = sc_results$estimate)
+          sc_results_df
+        },
+        error = function(e){
+          # print file name and dimension that are behaving unexpectedly
+          cat(paste("Results for spearman correlation will be filled with NA.\n\tTranscript: ",
+                    df$Event_ID, "\n\tDimension: ", dim, "\n", sep = ""))
+          # fill results with NA
+          sc_results_df <- data.frame(SpearR = NA)
+          sc_results_df
+        })
+      }
+
+      # function to calculate and format lagged Pearson correlation
+      calculate_lag_corr <- function(x_vars, y_vars, dim, align_var) {
+        # pull the values for each participant out of the data frame as a vectors
+        dim_x_vars <- x_vars[[dim]]
+        dim_y_vars <- y_vars[[dim]]
+
+        # get the amount of lag and lead
+        lagTimes <- lags[lags > 0]
+        leadTimes <- lags[lags < 0]
+        # set both at least equal to one so that the function works
+        if (length(lagTimes) == 0) {
+          lagTimes <- c(1)
+        }
+        if (length(leadTimes) == 0) {
+          leadTimes <- c(1)
+        }
+        # calculate lagged correlation
+        suppressWarnings({
+          lo_full <- YRmisc::cor.lag(dim_x_vars, dim_y_vars,
+                                     max(lagTimes), max(abs(leadTimes)))
+        })
+
+        # get the index of column name with zero
+        which0 <- which(colnames(lo_full) == "0")
+        # grab just the lag times, add a negative if a lead
+        colnames(lo_full)[0:which0] <- gsub("lag", "", colnames(lo_full)[0:which0])
+        # same for lead
+        colnames(lo_full)[which0:length(colnames(lo_full))] <- gsub("lead", "-", colnames(lo_full)[which0:length(colnames(lo_full))])
+        colnames(lo_full) <- paste0("PRho_lag_", colnames(lo_full))
+
+        # make a vector of names from the given lags
+        selectNames <- paste0("PRho_lag_", lags)
+        if (0 %in% lags) {selectNames <- append(selectNames, "0")}
+        # select columns given by user
+        lo_select <- lo_full %>%
+          dplyr::select(tidyselect::contains(selectNames))
+        lo_select
+      }
+
+      #iterate over variables to calculate spearman's correlation
+      dyad_dim_sc_list <- lapply(align_var, function(dim){
+        # run the trycatch function - either gives Rhos or NAs if there would be an issue
+        sc_results_df <- calculate_spearman_corr(x_vars, y_vars, dim, align_var)
+        # run the lagged covariance function
+        lag_results_df <- calculate_lag_corr(x_vars, y_vars, dim, align_var)
+        # put both results in  a single data frame
+        covar_results_df <- dplyr::bind_cols(sc_results_df, lag_results_df)
+
+        #add participant column only if it is the first iteration
+        colnames(covar_results_df) <- paste(colnames(covar_results_df), dim, sep = "-")
+        if (match(dim, align_var) == 1) {
+          covar_results_df$Participant_ID <- paste(participantvec, collapse = "---")
+        }
+        covar_results_df
+      })
+
+      #bind and add event id to spearman correlation df
+      dyad_covar <- dplyr::bind_cols(dyad_dim_sc_list, Event_ID = unique(df$Event_ID))
+      dyad_covar$Who_lagged <- participantvec[2]
+      # report the dyad data frame
+      dyad_covar
+    })
+    all_dyad_df <- dplyr::bind_rows(output_df_list)
+    return(all_dyad_df)
+  }
+
+  # Call the inner function and return its result
+  spearmans_corr_dyads(df_prep)
+}
